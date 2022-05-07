3203eb464241ab1aa4b17b016a3340922f05c170
chibicco <chibicco160.kino@gmail.com>
2022-05-07 11:31:20 +0900

    First commit

---
+++ javascript/default.js
@@ -0,0 +1,11 @@
+(function (f) {
+    s = document.createElement("script");
+    s.src = "//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
+    s.onload = function () {
+        f(jQuery.noConflict(true))
+    }
+    document.body.appendChild(s);
+}
+)(function ($) {
+    alert('2022年4月末をもちましてサービス終了いたしました。\n今までご愛顧いただきました皆様、SEGA様には深く御礼申し上げますとともに、この度のご案内となりましたことを心よりお詫び申し上げます。')
+});
